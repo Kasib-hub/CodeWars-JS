d3112db1c995fa7466f13cd00a671d5a5f91032c
Kasib <kasibzxc@gmail.com>
2022-06-01 22:43:05 -0400

    Update with comments

--- transformToPrime.js
+++ transformToPrime.js
@@ -1,3 +1,10 @@
+// 6 kyu
+// Transform To Prime
+
+// Given a List [] of n integers , find minimum number to be inserted in a list, so that sum of all elements of list should equal the closest prime number .
+
+// minimumNumber ({2,12,8,4,6}) ==> return (5)
+
 function isPrime(digit) {
     if (digit <= 1) {
         return false;
