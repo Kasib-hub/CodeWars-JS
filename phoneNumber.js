7b03c3d70c03f5472a52c1c28bffeedbdc014f08
Kasib <kasibabdullah8@gmail.com>
2022-07-13 03:30:47 -0400

    Formatting a string clever way. Shape of phone#

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,16 +1,11 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let ans = ''
-ans += '('
-let str = num.forEach((item, index) => {
-    ans += item
-    if (index == 2) {
-        
-        ans += ')'
-        ans += ' '
-    }
-    if (index == 5) {
-        ans += '-'
-    }
-})
+// Excellent method to outputting a method of a formatted string.
 
-console.log(ans.toString())
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+let format = '(xxx) xxx-xxxx'
+
+for (let i = 0; i < num.length; i++) {
+    format = format.replace('x', num[i])
+}
+
+console.log(format)
